05c64254bbe1bd1f27fcde4f7a7646c3aca78991
Saathveekan Satheeshkumar <saathveekan.satheeshkumar@kcl.ac.uk>
2023-06-10 10:21:34 +0100

    Remove .ics files from git

--- backend/src/main/java/com/farecompare/backend/service/serviceImpl/FileUploadServiceImpl.java
+++ backend/src/main/java/com/farecompare/backend/service/serviceImpl/FileUploadServiceImpl.java
@@ -5,8 +5,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import com.farecompare.backend.CalendarParser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,28 +18,24 @@ import com.farecompare.backend.service.FileUploadService;
 @Service
 public class FileUploadServiceImpl implements FileUploadService {
     
-    private String uploadFolderPath = "/home/saath/Dev/fareCompare/backend/src/test/resources/";
+//    private String uploadFolderPath = "/home/saath/Dev/fareCompare/backend/src/test/resources/";
     @Override
     public Map<String, String> uploadToLocal(MultipartFile file){
-        
+            Map<String, String> payload = new HashMap<>();
         try{
             byte[] data = file.getBytes();
-            System.out.println("ln22");
             String calendarData= new String(data);
-            System.out.println("calendarData: " + calendarData);
-            String pathDir = uploadFolderPath + file.getOriginalFilename();
-            System.out.println("pathDir: " + pathDir);
-            Path path = Paths.get(pathDir);
-            System.out.println("ln26");
-            Files.write(path, data);
-            System.out.println("ln28");
-            Map<String, String> payload = new HashMap<>();
+            System.out.println("Successful Upload");
+            CalendarParser calendarParser = new CalendarParser(calendarData);
+            System.out.println(calendarParser.sayHello());
+            List<String> allEvents = calendarParser.getEvents(calendarData);
+            System.out.println("allEvents: "+ allEvents);
+            System.out.println("Number of Events: " +  calendarParser.getNumberOfEvents());
             payload.put("message", calendarData);
 		    return payload;
         }
         catch(IOException e){
             e.printStackTrace();
-            Map<String, String> payload = new HashMap<>();
             payload.put("message", "Something went wrong!");
             return payload;
         }
